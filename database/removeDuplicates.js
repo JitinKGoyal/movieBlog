a451892aa086b698628b0e031cf70a3a17a87f05
JitinKGoyal <jkgoyal85@gmail.com>
2023-10-17 13:39:39 +0530

    removed duplicate movies

--- database/removeDuplicates.js
+++ database/removeDuplicates.js
@@ -1,15 +1,41 @@
-const list = require('./moviesList.json')
+// const list = require('./moviesList.json')
+const movieData = require('./moviesData.json')
 const fs = require('fs');
 
-// console.log(list.length)
-const newList = JSON.stringify([...new Set(list)])
-const filePath = 'newList.json'
 
-fs.writeFile(filePath, newList, (err) => {
-    if (err) {
-        console.error('Error writing to the file:', err);
-    } else {
-        console.log(`Data has been written to ${filePath}`);
-    }
-});
+function writeFile(params) {
 
+    // console.log(list.length)
+    const newList = JSON.stringify([...new Set(list)])
+    const filePath = 'newList.json'
+
+    fs.writeFile(filePath, newList, (err) => {
+        if (err) {
+            console.error('Error writing to the file:', err);
+        } else {
+            console.log(`Data has been written to ${filePath}`);
+        }
+    });
+
+}
+
+const removeDuplicateJsons = () => {
+    let newArray = [];
+    movieData.forEach((movie, i) => {
+        let exist = newArray.find(e => e.title === movie.title)
+        exist && console.log('loading...', i, exist?.title);
+        !exist && newArray.push(movie)
+    })
+
+    console.log(newArray.length)
+    const filePath = 'newListWithoutDuplicate.json'
+    fs.writeFile(filePath, JSON.stringify(newArray), (err) => {
+        if (err) {
+            console.error('Error writing to the file:', err);
+        } else {
+            console.log(`Data has been written to ${filePath}`);
+        }
+    });
+}
+
+removeDuplicateJsons()
