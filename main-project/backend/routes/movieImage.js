8168ce160291a3177f2be16b3e6096fd636f127c
JitinKGoyal <jkgoyal85@gmail.com>
2023-10-24 08:05:47 +0530

    font, getOnlineImages

--- main-project/backend/routes/movieImage.js
+++ main-project/backend/routes/movieImage.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getAllImagesController, searchImagesController } = require('../controllers/movieImageController');
+const { getAllImagesController, searchImagesController, getOnlineImagesDataForStorageController } = require('../controllers/movieImageController');
 
 // To get all images
 router.get('/', getAllImagesController);
 
 // To get images by search
 router.get('/:query/:offset/:limit', searchImagesController);
+
+router.get('/fetchFromWeb/:query', getOnlineImagesDataForStorageController)
 
 module.exports = router;
